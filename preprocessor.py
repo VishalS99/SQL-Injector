2266ece1374c343aa352628482881c7d740c77d3
VishalS99 <s-vishal1999@hotmail.com>
2019-12-13 17:15:09 +0530

    Added preprocessing

---
+++ preprocessor.py
@@ -0,0 +1,61 @@
+import cv2
+import numpy as np
+
+def image_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+def image_blur(image):
+    return cv2.bilateralFilter(image, 9, 75, 75)
+
+def image_thresholding(image):
+    return cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+
+def image_canny(image):
+    v = np.median(image)
+ 
+    lower = int(max(0, (1.0 - 0.33) * v))
+    upper = int(min(255, (1.0 + 0.33) * v))
+    return cv2.Canny(image, lower, upper)
+
+
+def detect_rect(image):
+    contours = cv2.findContours(image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE) [0]
+    
+    contours = sorted(
+                    contours,
+                    key=cv2.contourArea,
+                    reverse=True
+                )[:5]
+    
+    return cv2.boundingRect(contours[0])
+    
+
+
+def main():
+
+    initial_image = cv2.imread("./images/t2.jpg")
+    
+    cv2.namedWindow("Initial", cv2.WINDOW_NORMAL)
+    cv2.imshow("Initial", initial_image)
+    cv2.waitKey(0)
+    
+    gray = image_grayscale(initial_image)
+    
+    blur = image_blur(gray)
+    
+    thresh = image_thresholding(blur)
+    
+    canny = image_canny(thresh)
+    
+    [x, y, w, h] = detect_rect(canny)
+
+    final_image = initial_image.copy()
+    cv2.rectangle(final_image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    
+    cv2.namedWindow("Final", cv2.WINDOW_NORMAL)
+    cv2.imshow("Final", final_image)
+    cv2.waitKey(0)
+    
+
+if __name__ == "__main__":
+    main()
